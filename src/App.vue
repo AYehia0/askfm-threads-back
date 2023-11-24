6a9e6d712e7e2e9eefa99e4320c69bc3b2d86c53
AYehia0 <ahmedyehia611@gmail.com>
2023-11-24 20:27:49 +0200

    Initial vite + vue app

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+    import ShowTitle from './components/ShowTitle.vue'
+</script>
+
+<template>
+  <ShowTitle msg="Get Threads Back!" />
+</template>
---
+++ src/components/ShowTitle.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    defineProps<{ msg: string }>()
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <p>
+    How does this thing work ?
+    <a href="https://ayehia0.github.io/posts/askfm_reverse_engineering/" target="_blank"
+      >more info</a
+    >
+  </p>
+</template>
