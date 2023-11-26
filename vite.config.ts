3f103c8550d7b4f1c91e6f32fe9af1f4e12db9ed
AYehia0 <ahmedyehia611@gmail.com>
2023-11-26 23:01:49 +0200

    Updated things and added uhhmm idk things

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import path from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import webExtension, { readJsonFile } from "vite-plugin-web-extension";
+
+function generateManifest() {
+  const manifest = readJsonFile("src/manifest.json");
+  const pkg = readJsonFile("package.json");
+  return {
+    name: pkg.name,
+    description: pkg.description,
+    version: pkg.version,
+    ...manifest,
+  };
+}
+
+function root(...paths: string[]): string {
+  return path.resolve(__dirname, ...paths);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+    build: {
+        outDir: root("dist"),
+        emptyOutDir: true,
+    },
+    plugins: [
+        vue(),
+        webExtension({
+        manifest: generateManifest,
+        watchFilePaths: ["package.json", "manifest.json"],
+    }),
+  ],
+});
